b543b0129f21d06c9abc3c3d79b09900c8580b62
Zach <95zmartin@gmail.com>
2019-01-05 12:43:21 -0500

    Added event listener for add employee submit

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
+// Javascript for Timesheet Application
+
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyCIFxptqsAxM4-0TZPc2-5gjUbTpn8xA-A",
-authDomain: "test-project-1-c763b.firebaseapp.com",
-databaseURL: "https://test-project-1-c763b.firebaseio.com",
-projectId: "test-project-1-c763b",
-storageBucket: "test-project-1-c763b.appspot.com",
-messagingSenderId: "597513869158"
+    apiKey: "AIzaSyCIFxptqsAxM4-0TZPc2-5gjUbTpn8xA-A",
+    authDomain: "test-project-1-c763b.firebaseapp.com",
+    databaseURL: "https://test-project-1-c763b.firebaseio.com",
+    projectId: "test-project-1-c763b",
+    storageBucket: "test-project-1-c763b.appspot.com",
+    messagingSenderId: "597513869158"
 };
+
 firebase.initializeApp(config);
+
+// Initialize New Employee Variables
+var employee = "";
+var role = "";
+var startDate = "";
+var montlyRate = "";
+
+// Add New Employee
+$("#add-employee-btn").on("click", function(event) {
+
+    event.preventDefault();
+
+    // Values from Add Employee Inputs
+    employee = $("#employee-name-input").val().trim();
+    role = $("#role-input").val().trim();
+    startDate = $("#start-input").val().trim();
+    monthlyRate = $("#rate-input").val().trim();
+
+    console.log(employee);
+    console.log(role);
+    console.log(startDate);
+    console.log(monthlyRate);
+
+})
