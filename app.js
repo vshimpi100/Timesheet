bbfd3c9033631231643aa7c7a9fb5fd987dcd6e7
Vivek Shimpi <vshimpi100@gmail.com>
2019-01-05 12:15:01 -0500

    firebase initialized

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCIFxptqsAxM4-0TZPc2-5gjUbTpn8xA-A",
+authDomain: "test-project-1-c763b.firebaseapp.com",
+databaseURL: "https://test-project-1-c763b.firebaseio.com",
+projectId: "test-project-1-c763b",
+storageBucket: "test-project-1-c763b.appspot.com",
+messagingSenderId: "597513869158"
+};
+firebase.initializeApp(config);
